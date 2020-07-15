5274b115be36618b4117f65209fab2d72aa0a2ec
Anderson de Souza <anderson17ads@hotmail.com>
2020-07-15 02:22:19 -0300

    Create routes to crud

--- src/api/todo/todo.js
+++ src/api/todo/todo.js
@@ -6,3 +6,5 @@ const todoSchema = new mongoose.Schema({
     done: { type: Boolean, required: true, default: false },
     createAt: { type: Date, default: Date.now }
 })
+
+module.exports = restful.model('Todo', todoSchema)
--- src/config/database.js
+++ src/config/database.js
@@ -3,5 +3,9 @@ mongoose.Promise = global.Promise
 
 module.exports = mongoose.connect(
     'mongodb://localhost/todo',
-    { useNewUrlParser: true, useUnifiedTopology: true }
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false
+    }
 )
